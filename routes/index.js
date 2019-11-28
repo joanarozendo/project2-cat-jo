2dd4697c45839fd8c4956cbedf534635a7fc9a42
Joana Rozendo <joanarozendo@gmail.com>
2019-11-28 16:13:01 +0000

    minor style changes and name change

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const { Router } = require('express');
 const router = new Router();
 
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Music Inn' });
+  res.render('index', { title: 'Jam Up' });
 });
 
 router.get('/about', (req, res, next) => {
